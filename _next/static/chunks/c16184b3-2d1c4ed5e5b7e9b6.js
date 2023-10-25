92102ee502c8e6f07f1f4225ea63a70485eb18c5
kaialex22 <kalexinjp@gmail.com>
2023-10-25 22:35:00 +0900

    commit html

---
+++ _next/static/chunks/c16184b3-2d1c4ed5e5b7e9b6.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[976],{9417:function(a,b,c){c.d(b,{EyR:function(){return d}});var d={prefix:"fas",iconName:"angle-left",icon:[320,512,[8249],"f104","M41.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 256 246.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"]}}}])
